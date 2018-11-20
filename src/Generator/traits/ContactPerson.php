1c65c7cf6ee86aeceaeaf090d0a04c1a9cdae840
Sascha Westfeld <swestfeld@gmail.com>
2018-11-20 15:51:47 +0100

    fix mailaddress, phone and fax

--- src/Generator/traits/ContactPerson.php
+++ src/Generator/traits/ContactPerson.php
@@ -4,97 +4,120 @@ namespace EDI\Generator\Traits;
 
 /**
  * Trait ContactPerson
+ *
  * @package EDI\Generator\Traits
  */
 trait ContactPerson
 {
-    /** @var array */
-    protected $contactPerson;
-    /** @var array */
-    protected $mailAddress;
-    /** @var array */
-    protected $phoneNumber;
-    /** @var array */
-    protected $faxNumber;
+  /** @var array */
+  protected $contactPerson;
+  /** @var array */
+  protected $mailAddress;
+  /** @var array */
+  protected $phoneNumber;
+  /** @var array */
+  protected $faxNumber;
 
-    /**
-     * @return array
-     */
-    public function getContactPerson()
-    {
-        return $this->contactPerson;
-    }
+  /**
+   * @return array
+   */
+  public function getContactPerson()
+  {
+    return $this->contactPerson;
+  }
 
-    /**
-     * @param string $contactPerson
-     * @param string $section unused
-     * @return $this
-     */
-    public function setContactPerson($contactPerson, $section = '')
-    {
-        $this->contactPerson = [
-            'CTA',
-            $section,
-            [
-                $section,
-                $contactPerson,
-            ],
-        ];
+  /**
+   * @param string $contactPerson
+   * @param string $section unused
+   *
+   * @return $this
+   */
+  public function setContactPerson($contactPerson, $section = '')
+  {
+    $this->contactPerson = [
+      'CTA',
+      $section,
+      [
+        $section,
+        $contactPerson,
+      ],
+    ];
 
-        return $this;
-    }
+    return $this;
+  }
 
-    /**
-     * @return array
-     */
-    public function getMailAddress()
-    {
-        return $this->mailAddress;
-    }
+  /**
+   * @return array
+   */
+  public function getMailAddress()
+  {
+    return $this->mailAddress;
+  }
 
-    /**
-     * @param string $mailAddress
-     * @return $this
-     */
-    public function setMailAddress($mailAddress)
-    {
-        $this->mailAddress = ['COM', $mailAddress, 'EM'];
-        return $this;
-    }
+  /**
+   * @param string $mailAddress
+   *
+   * @return $this
+   */
+  public function setMailAddress($mailAddress)
+  {
+    $this->mailAddress = [
+      'COM',
+      [
+        $mailAddress,
+        'EM',
+      ],
+    ];
+    return $this;
+  }
 
-    /**
-     * @return array
-     */
-    public function getPhoneNumber()
-    {
-        return $this->phoneNumber;
-    }
+  /**
+   * @return array
+   */
+  public function getPhoneNumber()
+  {
+    return $this->phoneNumber;
+  }
 
-    /**
-     * @param string $phoneNumber
-     * @return $this
-     */
-    public function setPhoneNumber($phoneNumber)
-    {
-        $this->phoneNumber = ['COM', $phoneNumber, 'TE'];
-        return $this;
-    }
+  /**
+   * @param string $phoneNumber
+   *
+   * @return $this
+   */
+  public function setPhoneNumber($phoneNumber)
+  {
+    $this->phoneNumber = [
+      'COM',
+      [
+        $phoneNumber,
+        'TE',
+      ],
+    ];
+    return $this;
+  }
 
-    /**
-     * @return array
-     */
-    public function getFaxNumber()
-    {
-        return $this->faxNumber;
-    }
+  /**
+   * @return array
+   */
+  public function getFaxNumber()
+  {
+    return $this->faxNumber;
+  }
 
-    /**
-     * @param string $faxNumber
-     * @return $this
-     */
-    public function setFaxNumber($faxNumber)
-    {
-        $this->faxNumber = ['COM', $faxNumber, 'FX'];
-        return $this;
-    }
+  /**
+   * @param string $faxNumber
+   *
+   * @return $this
+   */
+  public function setFaxNumber($faxNumber)
+  {
+    $this->faxNumber = [
+      'COM',
+      [
+        $faxNumber,
+        'FX',
+      ],
+    ];
+    return $this;
+  }
 }
