d4747d91c32e27ec719896ca6d155f63a5ff0490
Sascha Westfeld <swestfeld@gmail.com>
2021-03-18 16:18:06 +0100

    fixed test for order response

--- tests/GeneratorTest/OrdersTest.php
+++ tests/GeneratorTest/OrdersTest.php
@@ -71,7 +71,7 @@ class OrdersTest extends TestCase
             $message = str_replace("'", "'\n", $encoder->get());
 //            fwrite(STDOUT, "\n\nORDERS\n" . $message);
 
-            $this->assertContains('UNT+20', $message);
+            $this->assertContains('UNT+19', $message);
 
         } catch (EdifactException $e) {
             fwrite(STDOUT, "\n\nORDERS\n" . $e->getMessage());
@@ -82,7 +82,7 @@ class OrdersTest extends TestCase
     public function testFreeText()
     {
         $this->assertEquals(
-            'FTX+ORI++HAE:89+Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commo:do ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma:gnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, :ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat mas:sa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate \'',
+            'FTX+ORI++HAE::89+Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commo:do ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma:gnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, :ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat mas:sa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate \'',
             (new Encoder([
                 Orders::addFTXSegment(
                     'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.',
